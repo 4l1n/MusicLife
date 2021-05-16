4ff615b6d7d28b787e034a8ac64ed74c707ecc04
Alin Cristea <acristea63@gmail.com>
2021-05-17 01:03:22 +0200

    Deploy added to git!

---
+++ build/precache-manifest.764dae6988981ff9da99e36871e539fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "869ab0504b68c792c9c1",
+    "url": "/static/css/main.adf654a7.chunk.css"
+  },
+  {
+    "revision": "869ab0504b68c792c9c1",
+    "url": "/static/js/main.869ab050.chunk.js"
+  },
+  {
+    "revision": "61bd1f65cf1d3e4e609f",
+    "url": "/static/js/1.61bd1f65.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4692f790a1f7636073d58c4b8812b7fd",
+    "url": "/index.html"
+  }
+];
